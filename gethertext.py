62e52af83b715580c336387cc481489938ae4b2a
shooter <b2234913@gmail.com>
2017-07-17 09:37:27 +0800

    add gether word from each info file

---
+++ gethertext.py
@@ -0,0 +1,47 @@
+#coding:utf8
+import os
+import re
+import csv
+import jieba
+import json
+import codecs
+import gensim
+from gensim.models import word2vec
+import sys  
+
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
+def get_text(path):
+	f = open(path)
+	x = f.read().split('"')
+	#for i in range(1,len(x)-1,2):
+	
+	for i in range(1,3,2):
+		y=x[i].split(": ")[1]
+		
+	return y
+
+def utf_8_encoder(unicode_csv_data):
+	for line in unicode_csv_data:
+		yield line.encode('utf-8')
+	
+def main():
+	text = []
+	for id in range(5900,46000):
+		path = 'json/' + str(id) + '/info.json'
+		if os.path.exists(path) == True :
+			info_text = get_text(path)
+			value = '"' + str(id) + '",' + '"' + info_text + '"'
+			text.append(value)
+			print(value)
+			
+	with codecs.open( "label_1.csv", 'w',"utf-8")  as f:
+		for item in text:
+			f.write(item + '\n')
+		f.close()
+		
+		
+		
+if __name__ == "__main__":
+    main()
